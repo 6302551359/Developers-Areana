ebd03b8d6119585f6d2e59e9889d7451318e14f5
Pavan Kumar <139989130+6302551359@users.noreply.github.com>
2025-11-12 13:10:55 +0530

    Add files via upload

---
+++ Personal Information System.py
@@ -0,0 +1,9 @@
+#Basic  personal information system that can stores and display the user information
+a=input("Enter your Name:")
+b=int(input("Enter your Age:"))
+c=input("Enter your city:")
+d=input("Enter your Hobbies:")
+print("NaME =",a)
+print("AGE =",b)
+print("CITY =",c)
+print("HOBBIES =",d)
